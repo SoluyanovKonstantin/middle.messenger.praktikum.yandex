2da4c0e5ed58b6241f879f430ad8ae90483ef226
Konstantin Soluianov <dr.chtec@yandex.ru>
2023-06-15 17:08:37 +0400

    added linters

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
 import { resolve } from 'node:path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default {
-  build: {
-    rollupOptions: {
-      input: resolve(__dirname, 'index.html'),
-    },
-  }
-}
+    build: {
+        rollupOptions: {
+            input: resolve(__dirname, 'index.html'),
+        },
+    }
+};
