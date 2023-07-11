6619ba2be6a7f42f1a095e07e4c5d687f9d29a5e
Konstantin Soluianov <dr.chtec@yandex.ru>
2023-07-11 20:59:00 +0400

    added router

--- utils/templateEngine.ts
+++ utils/templateEngine.ts
@@ -12,6 +12,40 @@ export class TemplateEngine {
         this.style = style;
     }
 
+    parseTemplate() {
+        const tagsInTemplateRegExp = /(<[^/][\s\S]*?>)/g;
+        const regExpResult = tagsInTemplateRegExp.exec(this.template);
+        const templateCopy = this.template;
+        const index = regExpResult?.index;
+        const endOfTagIndex = Number(index) + Number(regExpResult?.[1]?.length);
+        let templateAfterTag = templateCopy.slice(endOfTagIndex);
+        const templateAfterTagCopy = templateAfterTag;
+        const tag = regExpResult?.[1].slice(1,-1).split(' ').filter(el => el !== '\n' && el !== '');
+        const closingTagRegExp = new RegExp(`</${tag?.[0]}>`);
+        let isContinue = true;
+        let templateInsideTag = '';
+        let shift = 0;
+        while (isContinue) {
+            const closingTag = closingTagRegExp.exec(templateAfterTag);
+            templateInsideTag = templateAfterTagCopy.slice(0, Number(closingTag?.index) + shift);
+            const reg = new RegExp(`<${tag?.[0]}>`, 'g');
+            if (!templateAfterTag.match(reg)?.length) {
+                isContinue = false;
+            } else {
+                shift = Number(closingTag?.index) + Number(closingTag?.[0].length);
+                templateAfterTag =  templateAfterTag.slice(shift);
+            }
+        }
+
+        if (tag) {
+            const el = document.createElement(tag[0]);
+            tag.slice(1).forEach(attr => {
+                const [ key, value = '' ] = attr.split('=');
+                el.setAttribute(key, value);
+            });
+        }
+    }
+
 
     compile(variables: Props, components: Record<string, HTMLElement> = {}) {
         const variableInTemplateRegExp = /\{\{.*?\}\}/g;
