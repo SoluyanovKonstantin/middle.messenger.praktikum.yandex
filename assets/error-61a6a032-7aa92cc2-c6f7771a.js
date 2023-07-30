28afd27898a1298835fd91dcc9c3e5c648842fab
Konstantin Soluianov <dr.chtec@yandex.ru>
2023-07-31 00:13:20 +0400

    try

--- assets/error-61a6a032-7aa92cc2-c6f7771a.js
+++
@@ -1,6 +0,0 @@
-import{d as t}from"./block-67ee72f6-f6845175-8b95902b.js";const n=`
-<main class="body">
-    <h1>{{ errorCode }}</h1>
-</main>
-`,o=`.body{display:flex;flex-direction:row;justify-content:center;align-items:center;height:100vh;margin:0;box-sizing:border-box;font-size:50px}
-`;class e extends t{constructor(){const r=new URLSearchParams(window.location.search).get("errorCode")??"500";super("chat-component",{errorCode:r},n,o),e._style=o}}export{e as ErrorComponent};
