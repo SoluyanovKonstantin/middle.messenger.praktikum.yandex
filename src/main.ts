4d47397e35f770474d22d9d35fcb7af956eb6a41
Konstantin Soluianov <dr.chtec@yandex.ru>
2023-07-30 23:15:17 +0400

    try

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import './style.css';
-import router from '../utils/router';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore  
+import router from '/utils/router';
 import { RegistrationComponent } from './pages/registration/registration';
 import { AuthComponent } from './pages/auth/auth';
 import { ChatComponent } from './pages/chats/chat';
