3c9258c14a62fe3e5e0473f9265cf3d846f2522c
Konstantin Soluianov <dr.chtec@yandex.ru>
2023-07-30 22:54:16 +0400

    remove _http from webSocketApi

--- src/api/websocket.api.ts
+++ src/api/websocket.api.ts
@@ -1,11 +1,7 @@
-import { HTTPTransport } from '../../utils/http';
-
 export class WebsocketApi {
     private _url: string;
-    private _http: HTTPTransport;
     constructor() {
         this._url = import.meta.env.VITE_WS_URL;
-        this._http = new HTTPTransport();
     }
 
     connectChat(userId: string, chatId: string, token: string) {
