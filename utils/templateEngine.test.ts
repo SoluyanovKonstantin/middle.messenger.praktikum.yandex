5a67cd5009b947e8d465504ebbbe3575b41f9ede
Konstantin Soluianov <dr.chtec@yandex.ru>
2023-08-07 23:38:40 +0400

    tests for templateEngine

---
+++ utils/templateEngine.test.ts
@@ -0,0 +1,32 @@
+import { expect } from 'chai';
+import { TemplateEngine } from './templateEngine.js';
+
+describe('templateEngine', () => {
+
+    it('check that templateEngine returns', () => {
+        const templateEngine = new TemplateEngine('<div>{{ variable }}</div>', 'styles');
+
+        const page = templateEngine.compile({variable: 'dqwdqwdqw dqwdqwd'}).template;
+        expect(page instanceof HTMLElement).equal(true);
+    });
+
+    it('check variables in template engine', () => {
+        const templateEngine = new TemplateEngine('<div>{{ variable }}</div>', 'styles');
+
+        const page = templateEngine.compile({variable: 'dqwdqwdqw dqwdqwd'}).template;
+        expect(page.textContent).equal('dqwdqwdqw dqwdqwd');
+    });
+
+    it('check loops in template engine', () => {
+        const templateEngine = new TemplateEngine('<div for="let chat of chats" class="chat-preview">{{ chat.id }}</div>', 'styles');
+
+        const page = templateEngine.compile({variable: 'dqwdqwdqw dqwdqwd', 
+            arrays: {
+                chats: [{
+                    id: 202,
+                    title: 'Чат'
+                }],
+            }}).template;
+        expect(page.textContent).equal('202');
+    });
+});
